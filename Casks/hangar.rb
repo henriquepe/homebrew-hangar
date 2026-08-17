db81dcb61596a6c9a8490420211871d2fb12f24a
Henrique Pires <henrique.p.espindola@gmail.com>
2026-08-17 08:39:06 -0300

    hangar 0.2.2

--- Casks/hangar.rb
+++ Casks/hangar.rb
@@ -1,6 +1,6 @@
 cask "hangar" do
-  version "0.2.1"
-  sha256 "02e8e8083d800fba187ab5bdeada767a6102b4fb3be9b114427440ecc8b472eb"
+  version "0.2.2"
+  sha256 "becc07711e3c51b3f855d67ea8a948cf67cd0415cda656f98a3abecaa519dc28"
 
   url "https://github.com/henriquepe/homebrew-hangar/releases/download/v#{version}/Hangar-#{version}.dmg"
   name "Hangar"
